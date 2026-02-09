9cb9373004210cb3e949cb85b4735c5c4bfde13c
shobinn24 <shobinn24@gmail.com>
2026-02-09 13:22:40 -0500

    Add index and model routes

--- server/app.py
+++ server/app.py
@@ -1,1 +1,32 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# List of existing car models
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+@app.route('/')
+def index():
+    """Default route that welcomes users to the Flatiron Cars application."""
+    return 'Welcome to Flatiron Cars'
+
+@app.route('/<model>')
+def model(model):
+    """
+    Dynamic route that accepts a model name and checks if it exists.
+
+    Args:
+        model (str): The car model name provided in the URL
+        
+    Returns:
+        str: A message indicating whether the model exists or not  
+    """
+    if model in existing_models:
+        return f'Flatiron {model} is in our fleet!'
+    else:
+        return f'No models called {model} exists in our catalog'
+    
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
