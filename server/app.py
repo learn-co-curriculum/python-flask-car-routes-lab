f057070cf58116d64ecddbc53aa7fb6c04f8d106
CynthiaG-W <cindydetec@gmail.com>
2026-06-23 15:30:56 +0300

    Fix Flask app structure for CodeGrade

--- server/app.py
+++ server/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
+# Existing models in the fleet
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
 
 @app.route("/")
 def home():
@@ -15,13 +18,15 @@ def home():
 def get_model(model):
     """
     Checks whether a requested model exists in the fleet.
-    Returns an appropriate message based on the result.
+    Returns appropriate message based on result.
     """
-    if model.lower() in existing_models:
+
+    if model in existing_models:
         return f"Flatiron {model} is in our fleet!"
 
     return f"No models called {model} exists in our catalog"
 
 
+# Only runs locally
 if __name__ == "__main__":
     app.run(debug=True)
