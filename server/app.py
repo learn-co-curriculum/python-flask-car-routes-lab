833359d174e10c6593ade526a9e47ac09f597f66
Jairus <bg.jairtom098@gmai.com>
2026-07-01 11:22:54 +0300

    Add default and model-specific routes

--- server/app.py
+++ server/app.py
@@ -1,1 +1,24 @@
+from flask import Flask
+
+# List of car models currently in the Flatiron Cars fleet.
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    '''Default route: introduces the company.'''
+    return 'Welcome to Flatiron Cars'
+
+
+@app.route('/<model>')
+def model(model):
+    '''Model-specific route: reports whether the requested model is in the fleet.'''
+    if model in existing_models:
+        return f'Flatiron {model} is in our fleet!'
+    return f'No models called {model} exists in our catalog'
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
