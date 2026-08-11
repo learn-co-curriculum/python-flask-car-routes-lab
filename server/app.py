22ad866b40bd5b300d994f07b800f7c590bc8b29
Mark Njoroge <marknjoroge591@gmail.com>
2026-08-11 23:59:59 +0300

    Add car routes

--- server/app.py
+++ server/app.py
@@ -1,1 +1,25 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Models currently in the Flatiron Cars fleet
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+
+@app.route('/')
+def index():
+    # Default route - welcomes visitors to the site
+    return "Welcome to Flatiron Cars"
+
+
+@app.route('/<model>')
+def get_model(model):
+    # Checks if the requested model exists in our fleet
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog"
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
