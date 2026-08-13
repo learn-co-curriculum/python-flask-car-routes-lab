1af5f44566f064e69cac724f20300e5f6dc875aa
Wesley Wanga <wesleyjwanga@gmail.com>
2026-08-13 19:42:23 +0300

    Add car company Flask routes

--- server/app.py
+++ server/app.py
@@ -1,1 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+# Default route
+@app.route("/")
+def index():
+    return "Welcome to Flatiron Cars"
+
+# Route for requesting a specific car model
+@app.route("/<model>")
+def model(model):
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
